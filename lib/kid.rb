87f636b79b4bf60ee317adbc49b29cd178c9ba48
Olivia Cubela <oliviacubela@gmail.com>
2020-07-16 00:54:24 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,12 +2,6 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  include Dance
-  extend Metadancing
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
