16e4df5fa89a904c375e8f1a62a55601d67308b5
Olivia Cubela <oliviacubela@gmail.com>
2020-07-16 01:08:18 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,6 +3,14 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+  
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
