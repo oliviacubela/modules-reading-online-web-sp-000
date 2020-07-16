27ce4eee43e975b147e91c149b27e45350467052
Olivia Cubela <oliviacubela@gmail.com>
2020-07-16 00:56:36 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,24 +1,26 @@
-module FancyDance               #<==defined our 'FancyDance' module
-  module InstanceMethods        #<==defined second module 'InstanceMethods' withing 'FancyDance' module
+module FancyDance
+  module InstanceMethods
+ 
     def twirl
-      "I'm twirling!"              #Within 'InstanceMethods', we place all our methods that we intend to be used as instance methods
+      "I'm twirling!"
     end
-
+ 
     def jump
       "Look how high I'm jumping!"
     end
-
+ 
     def pirouette
       "I'm doing a pirouette"
     end
-
+ 
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
-
-  module ClassMethods           #Here, we define a second nested module (nested inside of 'FancyDance') called 'ClassMethods'
-    def metadata                #placed the 'metadata' method inside, to be used as a class method.
+ 
+  module ClassMethods
+ 
+    def metadata
       "This class produces objects that love to dance."
     end
   end
