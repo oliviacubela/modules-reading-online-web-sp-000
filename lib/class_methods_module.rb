2eb38d9d2fe69ff40027069d3ac41a9f1814601c
Olivia Cubela <oliviacubela@gmail.com>
2020-07-16 00:38:36 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -4,11 +4,11 @@ module Metadancing
     "This class produces objects that love to dance."
   end
 
-  class Kid
-    extend Metadancing
-  end
-
-  class Dancer
-    extend Metadancing
-  end
+  # class Kid
+  #   extend Metadancing
+  # end
+  #
+  # class Dancer
+  #   extend Metadancing
+  # end
 end
